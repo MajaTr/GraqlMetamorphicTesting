9598655c5144bd7d6e804edd4aa921217fd85929
msaska9 <molnar.saska.zoltan@gmail.com>
2020-02-21 22:14:10 +0000

    Subset testing with rules.

---
+++ src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/subset_tests/RulesSubsetSchema.java
@@ -0,0 +1,44 @@
+package uk.ac.cam.gp.charlie.metamorphic.tests.subset_tests;
+
+import graql.lang.Graql;
+import graql.lang.query.GraqlQuery;
+import java.util.ArrayList;
+import java.util.List;
+import uk.ac.cam.gp.charlie.metamorphic.tests.SchemaGenerator;
+
+public class RulesSubsetSchema implements SchemaGenerator {
+
+  @Override
+  public List<GraqlQuery> generate(int seed) {
+    List<GraqlQuery> result = new ArrayList<>();
+    result.add(Graql.parse("define parentship sub relation, relates parent, relates child;"));
+    result.add(Graql.parse("define siblings sub relation, relates child;"));
+    result.add(Graql.parse("define grandparentship sub relation, relates grandparent, relates child;"));
+    result.add(Graql.parse("define cousins sub relation, relates cousin;"));
+    result.add(Graql.parse("define name sub attribute, datatype string;"));
+    result.add(Graql.parse("define person sub entity, plays child, plays parent, plays grandparent, plays cousin, has name;"));
+    result.add(Graql.parse("define people-with-same-parent-are-siblings sub rule, when {"
+        + "(parent: $m, child: $x) isa parentship;"
+        + "(parent: $m, child: $y) isa parentship;"
+        + "$x != $y;"
+        + "}, then {"
+        + "($x, $y) isa siblings;"
+        + "};"));
+    result.add(Graql.parse("define parent-of-parent-is-grandparent sub rule, when {"
+        + "(parent: $x, child: $y) isa parentship;"
+        + "(parent: $y, child: $z) isa parentship;"
+        + "}, then {"
+        + "(grandparent: $x, child: $z) isa grandparentship;"
+        + "};"));
+    result.add(Graql.parse("define people-with-same-grandparent-are-cousins sub rule, when {"
+        + "(grandparent: $g, child: $x) isa grandparentship;"
+        + "(grandparent: $g, child: $y) isa grandparentship;"
+        + "not {($x, $y) isa siblings;};"
+        + "$x != $y;"
+        + "}, then {"
+        + "($x, $y) isa cousins;"
+        + "};"));
+
+    return result;
+  }
+}
---
+++ src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/subset_tests/RulesSubsetTest.java
@@ -0,0 +1,66 @@
+package uk.ac.cam.gp.charlie.metamorphic.tests.subset_tests;
+
+import static graql.lang.Graql.var;
+
+import graql.lang.Graql;
+import graql.lang.query.GraqlQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import uk.ac.cam.gp.charlie.metamorphic.Utils.DebugPrinter;
+import uk.ac.cam.gp.charlie.metamorphic.properties.EqProperty;
+import uk.ac.cam.gp.charlie.metamorphic.properties.Property;
+import uk.ac.cam.gp.charlie.metamorphic.properties.SubSetProperty;
+import uk.ac.cam.gp.charlie.metamorphic.tests.SchemaGenerator;
+import uk.ac.cam.gp.charlie.metamorphic.tests.TestGenerator;
+
+public class RulesSubsetTest implements TestGenerator {
+
+  private int numNodes;
+
+  public RulesSubsetTest() { numNodes = 30; }
+
+  @Override
+  public List<GraqlQuery> generate(int seed) {
+    Random random = new Random(seed);
+    List<GraqlQuery> result = new ArrayList<>();
+
+    for (int i = 0; i < numNodes; i++) {
+      result.add(Graql.parse("insert $x isa person, has name \"i" + i + "\";"));
+    }
+
+    for (int i = 1; i < numNodes; i++) {
+      int parent = random.nextInt(i);
+      result.add(Graql.match(
+          var("x").isa("person").has("name", "i" + parent),
+          var("y").isa("person").has("name", "i" + i)
+      ).insert(
+          var("edge").isa("parentship").rel("parent", "x").rel("child", "y")
+      ));
+    }
+
+    result.add(Graql.match(
+        var("C").isa("cousins").rel(var("c")).rel(var("c2")),
+        var("P").isa("parentship").rel("parent", var("u")).rel("child", var("c2")),
+        var("u").has("name", var("unclename")),
+        var("c").has("name", var("childname"))
+    ).get("unclename", "childname"));
+
+    result.add(Graql.match(
+        var("P").isa("parentship").rel("parent", var("p")).rel("child", var("c")),
+        var("S").isa("siblings").rel(var("p")).rel(var("u")),
+        var("u").has("name", var("unclename")),
+        var("c").has("name", var("childname"))
+    ).get("unclename", "childname"));
+
+    return result;
+  }
+
+  @Override
+  public Property getTestingProperty() { return new SubSetProperty(); }
+
+  @Override
+  public SchemaGenerator getSchemaGenerator() {
+    return new RulesSubsetSchema();
+  }
+}
