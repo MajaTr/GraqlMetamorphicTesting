b8de593b3a125cf42a61c45a6f98c34b036035b1
Stefi <sp928@cam.ac.uk>
2020-03-02 00:38:21 +0000

    added comments - stefi

--- src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/subset_tests/ExtendableQueriesSchema.java
+++ src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/subset_tests/ExtendableQueriesSchema.java
@@ -1,18 +1,22 @@
+/*
+    Definition of a layered schema that allows generating extendable queries.
+    It has a base entity "A" with a pre-defined nBSubEntities subentities B1, B2,...,
+    each B_i having one subentity Ci.
+ */
+
 package uk.ac.cam.gp.charlie.metamorphic.tests.subset_tests;
 
 import graql.lang.Graql;
 import graql.lang.query.GraqlQuery;
 import graql.lang.statement.StatementType;
-import uk.ac.cam.gp.charlie.metamorphic.Utils;
 import uk.ac.cam.gp.charlie.metamorphic.tests.SchemaGenerator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static graql.lang.Graql.type;
 
-public class ExtandableQueriesSchema implements SchemaGenerator {
+public class ExtendableQueriesSchema implements SchemaGenerator {
 
     private int nAttributes = 20;
     private int nBSubEntities = 20;
@@ -28,14 +32,11 @@ public class ExtandableQueriesSchema implements SchemaGenerator {
             result.add(Graql.define(type("attr" + i).sub("attribute").datatype("string")));
         }
 
-
         StatementType definitionA = type("A").sub("entity");
 
         for(int i = 0; i < nAttributes; i++){
             definitionA = definitionA.has("attr" + i);
         }
-
-
 
         result.add(Graql.define(definitionA));
 
--- src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/subset_tests/ExtendableQueriesTest.java
+++ src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/subset_tests/ExtendableQueriesTest.java
@@ -3,7 +3,6 @@ package uk.ac.cam.gp.charlie.metamorphic.tests.subset_tests;
 import graql.lang.Graql;
 import graql.lang.query.GraqlQuery;
 import graql.lang.statement.StatementInstance;
-import graql.lang.statement.StatementType;
 import uk.ac.cam.gp.charlie.metamorphic.Utils;
 import uk.ac.cam.gp.charlie.metamorphic.properties.Property;
 import uk.ac.cam.gp.charlie.metamorphic.properties.SubSetProperty;
@@ -16,21 +15,20 @@ import java.util.Random;
 
 import static graql.lang.Graql.var;
 
-public class ExtandableQueriesTest implements TestGenerator {
+public class ExtendableQueriesTest implements TestGenerator {
 
-    private String baseIsa ;
+    private String baseIsa;
     private List<String> baseAttributesValues;
     private int nAttributes = 20;
     private int nBSubEntities = 20;
     private int nInserts = 50;
 
+    //Generate a query asking for all the elements of a given entity, having
     private GraqlQuery generateQuery(String isaIn, List<String> attributesValues){
         StatementInstance si = var("v").isa(isaIn);
-
         for(int i = 0; i < attributesValues.size(); i++){
             si = si.has("attr" + i, attributesValues.get(i));
         }
-
 
         return Graql.match(si).get("v");
     }
@@ -77,16 +75,21 @@ public class ExtandableQueriesTest implements TestGenerator {
         List<GraqlQuery> result = generateInserts(seed);
 
         Random random = new Random(seed);
+        //The initial query will have A as the entity and nAttributes attributes
+        // called i, for i = 0 to nAttributes
         baseIsa = "A";
         baseAttributesValues = new ArrayList<>();
         for(int i = 0; i < nAttributes; i++){
             baseAttributesValues.add("" + i);
         }
 
-
         GraqlQuery baseQuery = generateQuery(baseIsa, baseAttributesValues);
         GraqlQuery extendedQuery;
         result.add(baseQuery);
+
+        //We can extend the base query by replacing the base entity A with either B or C
+        //(and we choose randomly).
+        //The result will be a subset of the result of the base query.
         boolean borc = random.nextBoolean();
         if(borc == true){
             extendedQuery = extendSubentityB(seed);
@@ -96,12 +99,9 @@ public class ExtandableQueriesTest implements TestGenerator {
         }
         result.add(extendedQuery);
 
-
-
         return result;
 
     }
-
 
     @Override
     public Property getTestingProperty() {
@@ -110,6 +110,6 @@ public class ExtandableQueriesTest implements TestGenerator {
 
     @Override
     public SchemaGenerator getSchemaGenerator() {
-        return new ExtandableQueriesSchema();
+        return new ExtendableQueriesSchema();
     }
 }
