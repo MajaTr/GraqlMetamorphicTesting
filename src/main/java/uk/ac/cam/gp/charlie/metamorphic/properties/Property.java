136d46658573cae5bb98b350ac5a8f8cdfb32fb5
MajaTr <maja.ada0@gmail.com>
2020-03-02 02:57:11 +0000

    Added comments to interfaces

--- src/main/java/uk/ac/cam/gp/charlie/metamorphic/properties/Property.java
+++ src/main/java/uk/ac/cam/gp/charlie/metamorphic/properties/Property.java
@@ -4,6 +4,14 @@ import grakn.client.answer.ConceptMap;
 
 import java.util.List;
 
+/*
+A property interface.
+
+test(answers) is supposed to take a list of results from "get" queries
+(a result from a single query is a List<ConceptMap>, therefore the argument is a List<List<ConceptMap>>)
+and return a boolean whether they hold the desired property.
+ */
+
 public interface Property {
     boolean test(List<List<ConceptMap>> answers);
 }
--- src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/TestGenerator.java
+++ src/main/java/uk/ac/cam/gp/charlie/metamorphic/tests/TestGenerator.java
@@ -5,6 +5,14 @@ import uk.ac.cam.gp.charlie.metamorphic.properties.Property;
 
 import java.util.List;
 
+/*
+A test interface.
+
+Each test consists of a schema, the data generator, and the property the results have to satisfy.
+
+generate(seed) is supposed to deterministically return a list of Graql queries to instantiate the data.
+However some definitions can be included in the TestGenerator instead of SchemaGenerator if it's more convenient.
+*/
 public interface TestGenerator {
     List<GraqlQuery> generate(int seed);
 
